4ce1fbd6c91985a24f38b7a38b0e8da9e0fcb187
abettername <123524182+abettername@users.noreply.github.com>
2023-02-21 23:28:12 -0600

    chapter 3 logo assignment

--- 03-critical-design/start/assets/js/main.js
+++ 03-critical-design/start/assets/js/main.js
@@ -4,39 +4,39 @@
 
 // 2
 document.getElementById("ele2").addEventListener("click", function () {
-  if (this.src.includes("cat-01")){
-    this.src = "assets/img/cat-02.jpg";
+  if (this.src.includes("logo-original")){
+    this.src = "assets/img/logoF.jpg";
   } else {
-    this.src = "assets/img/cat-01.jpg";
+    this.src = "assets/img/logo-original.png";
   }
 });
 
 // 3
 let ele3 = document.querySelector("#ele3");
 ele3.addEventListener("click", function () {
-  if (this.src.includes("cat-01")){
-    this.src = "assets/img/cat-02.jpg";
+  if (this.src.includes("logo-original")){
+    this.src = "assets/img/logoF.jpg";
   } else {
-    this.src = "assets/img/cat-01.jpg";
+    this.src = "assets/img/logo-original.png";
   }
 });
 
 // 4
 let ele4 = document.querySelector("#ele4");
 ele4.addEventListener("mouseover", function () {
-  this.src = "assets/img/cat-02.jpg";
+  this.src = "assets/img/logoF.jpg";
 });
 ele4.addEventListener("mouseout", function () {
-  this.src = "assets/img/cat-01.jpg";
+  this.src = "assets/img/logo-original.png";
 });
 
 // 5
 let ele5 = document.querySelector("#ele5");
 ele5.addEventListener("mouseover", function () {
-  this.style.backgroundImage = "url(assets/img/cat-02.jpg)";
+  this.style.backgroundImage = "url(assets/img/logoF.jpg)";
 });
 ele5.addEventListener("mouseout", function () {
-  this.style.backgroundImage = "url(assets/img/cat-01.jpg)";
+  this.style.backgroundImage = "url(assets/img/logo-original.png)";
 });
 
 
